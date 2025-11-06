2028a750088f7ec35a3cd00212d572fd171d6c79
Mickael Darmon <mickael.darmon@live.fr>
2025-11-06 12:25:25 +0100

    Migration des données depuis l'ordinateur après l'appel MongoDB

---
+++ migrate.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+from pymongo import MongoClient
+import time
+
+# Attendre un peu pour laisser Mongo démarrer
+time.sleep(5)
+
+# Connexion à MongoDB depuis localhost
+client = MongoClient("mongodb://admin:password@localhost:27017/")
+
+
+# Lire la variable d'environnement
+#mongo_uri = os.getenv("MONGO_URI", "mongodb://admin:password@mongo:27017/") 
+
+#client = MongoClient(mongo_uri)
+
+
+
+db = client["healthcare_db"]
+collection = db["patients"]
+print("Connexion à MongoDB réussie")
+
+if collection.count_documents({}) > 0:
+    collection.delete_many({})
+    print("Collection vidée")
+
+# Charger le CSV si localhost
+df = pd.read_csv("./data/healthcare-dataset.csv")
+print(f"{len(df)} lignes chargées depuis le CSV.")
+
+# Charger le CSV si docker
+#df = pd.read_csv("/app/data/healthcare-dataset.csv")
+#print(f"{len(df)} lignes chargées depuis le CSV.")
+
+# Convertir en dictionnaires et insérer dans MongoDB
+data = df.to_dict(orient="records")
+collection.insert_many(data,)
+print(f"{len(data)} documents insérés dans MongoDB")
