950d9453903ec1c89b60cf7ce9639f0c37f9310e
Mickael Darmon <mickael.darmon@live.fr>
2026-01-08 00:24:20 +0100

    Modification du code pour automatiser le téléchargement du csv

--- migrate.py
+++ migrate.py
@@ -2,6 +2,24 @@ import os
 import pandas as pd
 from pymongo import MongoClient
 import time
+import shutil
+import kagglehub
+
+# Télécharger le dataset
+print("Téléchargement du dataset depuis Kaggle...")
+path = kagglehub.dataset_download("prasad22/healthcare-dataset")
+print("Dataset téléchargé dans :", path)
+
+DEST_DIR = "./data"
+os.makedirs(DEST_DIR, exist_ok=True)
+
+file = 'healthcare_dataset.csv'
+src = os.path.join(path, file)
+print(src)
+dst = os.path.join(DEST_DIR, file)
+shutil.copy(src, dst)
+print(f"CSV copié vers {dst}")
+
 
 # Attendre un peu pour laisser Mongo démarrer
 time.sleep(5)
@@ -11,9 +29,9 @@ client = MongoClient("mongodb://admin:password@localhost:27017/")
 
 
 # Lire la variable d'environnement
-#mongo_uri = os.getenv("MONGO_URI", "mongodb://admin:password@mongo:27017/") 
+mongo_uri = os.getenv("MONGO_URI", "mongodb://admin:password@mongo:27017/") 
 
-#client = MongoClient(mongo_uri)
+client = MongoClient(mongo_uri)
 
 
 
@@ -26,12 +44,12 @@ if collection.count_documents({}) > 0:
     print("Collection vidée")
 
 # Charger le CSV si localhost
-df = pd.read_csv("./data/healthcare-dataset.csv")
-print(f"{len(df)} lignes chargées depuis le CSV.")
+#df = pd.read_csv("./data/healthcare_dataset.csv")
+#print(f"{len(df)} lignes chargées depuis le CSV.")
 
 # Charger le CSV si docker
-#df = pd.read_csv("/app/data/healthcare-dataset.csv")
-#print(f"{len(df)} lignes chargées depuis le CSV.")
+df = pd.read_csv("/app/data/healthcare_dataset.csv")
+print(f"{len(df)} lignes chargées depuis le CSV.")
 
 # Convertir en dictionnaires et insérer dans MongoDB
 data = df.to_dict(orient="records")
